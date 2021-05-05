6d1dc18d9afbd31e4b0e4c4b8389a7ba5c737790
Marco Antonio Molina López <marco.molina@segip.gob.bo>
2021-05-05 17:11:04 -0400

    Mejora del metodo: getListaTraspaso().

--- app/Http/Controllers/TraspasosController.php
+++ app/Http/Controllers/TraspasosController.php
@@ -13,7 +13,7 @@ class TraspasosController extends Controller
     public function getListaTraspaso($idEstudiante)
     {
         $arrayCamposSelect = [
-            'estudiante.id_estudiante as idEstudiante',
+            /* 'estudiante.id_estudiante as idEstudiante',
             'estudiante.ru',
             'estudiante.ci',
             'estudiante.complemento',
@@ -21,38 +21,48 @@ class TraspasosController extends Controller
             'estudiante.materno',
             'estudiante.nombres',
             'estudiante.fecha_nacimiento AS fechaNacimiento',
-            'estudiante.sexo',
+            'estudiante.sexo', */
 
-            'carrera.nombre as carreraOrigen',
+            'traspaso.id_traspaso as idTraspaso',
 
+            DB::raw('(select id_universidad AS idUniversidadDestino FROM universidad WHERE id_universidad = traspaso.id_univ_destino)'),
+            DB::raw('(select nombre AS nombreUniversidadDestino FROM universidad WHERE id_universidad = traspaso.id_univ_destino)'),
+            DB::raw('(select id_carrera AS idCarreraDestino FROM carrera WHERE id_carrera = traspaso.id_carrera_destino)'),
+            DB::raw('(select nombre AS nombreCarreraDestino FROM carrera WHERE id_carrera = traspaso.id_carrera_destino)'),
+
+            DB::raw("(select '01/2001' AS periodo)"),
+
+
+            'traspaso.fecha_solicitud AS fechaSolicitud',
+            'traspaso.motivo',
+/*
             'traspaso.id_traspaso AS idTraspaso',
             'traspaso.id_carrera_destino AS idCarreraDestino',
             'traspaso.descripcion AS descripcion',
             'traspaso.anio_ingreso AS anioIngreso',
             'traspaso.materias_aprobadas AS materiasAprobadas',
             'traspaso.materias_reprobadas AS materiasReprobadas',
-            'traspaso.fecha_solicitud AS fechaSolicitud',
-            'traspaso.motivo',
 
 
             'estudiante_tramite.fecha AS fechaProceso',
             'estudiante_tramite.observaciones',
 
             'tramite.id_tramite AS idTramite',
-            'tramite.descripcion AS tipoTramite',
-            'estado.descripcion AS estado'
+            'tramite.descripcion AS tipoTramite', */
+
+            'estado.id_estado AS estado'
         ];
 
 
 
         $dataComplementaria = DB::table('estudiante')
 
-            ->join('estudiante_carrera', 'estudiante_carrera.id_estudiante', '=' , 'estudiante.id_estudiante' )
-            ->join('carrera', 'carrera.id_carrera', '=', 'estudiante_carrera.id_carrera' )
+            // ->join('estudiante_carrera', 'estudiante_carrera.id_estudiante', '=' , 'estudiante.id_estudiante' )
+            // ->join('carrera', 'carrera.id_carrera', '=', 'estudiante_carrera.id_carrera' )
 
             ->join('traspaso', 'traspaso.id_estudiante', '=', 'estudiante.id_estudiante')
             ->join('estudiante_tramite', 'estudiante_tramite.id_estudiante', '=', 'estudiante.id_estudiante')
-            ->join('tramite', 'estudiante_tramite.id_tramite', '=', 'tramite.id_tramite')
+            // ->join('tramite', 'estudiante_tramite.id_tramite', '=', 'tramite.id_tramite')
             ->join('estado', 'estudiante_tramite.id_estado', '=', 'estado.id_estado')
             ->select( $arrayCamposSelect )
             ->where('estudiante.id_estudiante', '=', $idEstudiante)
