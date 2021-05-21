7d08780e1e020545b69f2b033fce4078e096e417
Marco Antonio Molina López <marco.molina@segip.gob.bo>
2021-05-20 22:02:58 -0400

    Creacion de la clase estatatica: Estado.

---
+++ app/utils/Estado.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\utils;
+
+class Estado{
+
+    public const DESACTIVADO  = 0;
+    public const ACTIVADO     = 1;
+    public const CREADO       = 2;
+    public const ENVIADO      = 3;
+    public const RECEPCIONADO = 4;
+    public const APROBADO     = 5;
+    public const RECHAZADO    = 6;
+    public const FINALIZADO   = 7;
+}
