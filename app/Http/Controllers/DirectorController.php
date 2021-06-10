8641de761948516d4e04791f6544737510ade33f
Marco Antonio Molina López <marco.molina@segip.gob.bo>
2021-06-10 19:15:27 -0400

    Se añadio el metodo: getTramitesAtendidos() en DirectorController

--- app/Http/Controllers/DirectorController.php
+++ app/Http/Controllers/DirectorController.php
@@ -209,4 +209,211 @@ class DirectorController extends Controller
             'error'   => null
         ]);
     }
+
+
+    public function getTramitesAtendidos($idCarrera){
+
+        $arrayEstados = [ Estado::RECHAZADO, Estado::FINALIZADO ];
+
+        $selectColumnsAnulaciones = [
+            'estudiante.id_estudiante as idEstudiante',
+            'estudiante.ru',
+            'estudiante.ci',
+            'estudiante.complemento',
+            'estudiante.paterno',
+            'estudiante.materno',
+            'estudiante.nombres',
+            'estudiante.fecha_nacimiento AS fechaNacimiento',
+            'estudiante.sexo',
+
+            'anulacion.id_anulacion AS idTipoTramite',
+            'anulacion.fecha_solicitud AS fechaSolicitud',
+            'anulacion.id_carrera_origen AS idCarreraOrigen',
+            'carrera.nombre AS carrera',
+            'anulacion.motivo',
+
+            'estudiante_tramite.id_estudiante_tramite as idEstudianteTipoTramiteTablaIntermedia',
+            'estudiante_tramite.fecha_proceso AS fechaProceso',
+            'estudiante_tramite.observaciones',
+
+            'tramite.id_tramite AS idTramite',
+            'tramite.descripcion AS tramite',
+
+            'estado.id_estado AS idEstado',
+            'estado.descripcion AS estado',
+
+            'entidad.id_entidad AS idEntidad',
+            'entidad.descripcion AS entidad'
+        ];
+
+        $estudianteAnulaciones = DB::table('estudiante')
+
+            ->join('estudiante_carrera', 'estudiante_carrera.id_estudiante', '=' , 'estudiante.id_estudiante')
+            ->join('carrera', 'carrera.id_carrera', '=' , 'estudiante_carrera.id_carrera')
+            ->join('estudiante_tramite', 'estudiante_tramite.id_estudiante', '=', 'estudiante.id_estudiante' )
+            ->join('anulacion', 'anulacion.id_anulacion', '=', 'estudiante_tramite.id_anulacion')
+
+            ->join('tramite', 'estudiante_tramite.id_tramite', '=', 'tramite.id_tramite')
+            ->join('estado', 'estudiante_tramite.id_estado', '=', 'estado.id_estado')
+            ->join('entidad', 'estudiante_tramite.id_entidad', '=', 'entidad.id_entidad')
+            ->select( $selectColumnsAnulaciones )
+            ->where( 'estudiante_tramite.id_entidad', '=' , Entidad::DIRECTOR_CARRERA_ORIGEN )
+            ->where( 'anulacion.id_carrera_origen', '=', $idCarrera)
+            ->where( 'estudiante_tramite.activo', '=' , true )
+            ->whereIn( 'estudiante_tramite.id_estado', $arrayEstados );
+
+
+
+
+
+        $selectColumnsCambioCarrera = [
+                'estudiante.id_estudiante as idEstudiante',
+                'estudiante.ru',
+                'estudiante.ci',
+                'estudiante.complemento',
+                'estudiante.paterno',
+                'estudiante.materno',
+                'estudiante.nombres',
+                'estudiante.fecha_nacimiento AS fechaNacimiento',
+                'estudiante.sexo',
+
+                'cambio_carrera.id_cambio_carrera AS idTipoTramite',
+                'cambio_carrera.fecha_solicitud AS fechaSolicitud',
+                'cambio_carrera.id_carrera_origen AS idCarreraOrigen',
+                'carrera.nombre AS carrera',
+                'cambio_carrera.motivo',
+
+                'estudiante_tramite.id_estudiante_tramite as idEstudianteTipoTramiteTablaIntermedia',
+                'estudiante_tramite.fecha_proceso AS fechaProceso',
+                'estudiante_tramite.observaciones',
+
+                'tramite.id_tramite AS idTramite',
+                'tramite.descripcion AS tramite',
+
+                'estado.id_estado AS idEstado',
+                'estado.descripcion AS estado',
+
+                'entidad.id_entidad AS idEntidad',
+                'entidad.descripcion AS entidad'
+        ];
+
+        $estudianteCambiosCarreraOrigen = DB::table('estudiante')
+            ->join('estudiante_carrera', 'estudiante_carrera.id_estudiante', '=' , 'estudiante.id_estudiante')
+            ->join('carrera', 'carrera.id_carrera', '=' , 'estudiante_carrera.id_carrera')
+            ->join('estudiante_tramite', 'estudiante_tramite.id_estudiante', '=', 'estudiante.id_estudiante' )
+            ->join('cambio_carrera', 'cambio_carrera.id_cambio_carrera', '=', 'estudiante_tramite.id_cambio_carrera')
+
+            ->join('tramite', 'estudiante_tramite.id_tramite', '=', 'tramite.id_tramite')
+            ->join('estado', 'estudiante_tramite.id_estado', '=', 'estado.id_estado')
+            ->join('entidad', 'estudiante_tramite.id_entidad', '=', 'entidad.id_entidad')
+            ->select( $selectColumnsCambioCarrera )
+            ->where( 'estudiante_tramite.id_entidad', '=' , Entidad::DIRECTOR_CARRERA_ORIGEN )
+            ->where( 'cambio_carrera.id_carrera_origen', '=', $idCarrera)
+            ->where( 'estudiante_tramite.activo', '=' , true )
+            ->whereIn( 'estudiante_tramite.id_estado', $arrayEstados )
+            ->orderBy( 'estudiante_tramite.fecha_proceso' , 'DESC');
+
+
+
+        $estudianteCambiosCarreraDestino = DB::table('estudiante')
+            ->join('estudiante_carrera', 'estudiante_carrera.id_estudiante', '=' , 'estudiante.id_estudiante')
+            ->join('carrera', 'carrera.id_carrera', '=' , 'estudiante_carrera.id_carrera')
+            ->join('estudiante_tramite', 'estudiante_tramite.id_estudiante', '=', 'estudiante.id_estudiante' )
+            ->join('cambio_carrera', 'cambio_carrera.id_cambio_carrera', '=', 'estudiante_tramite.id_cambio_carrera')
+
+            ->join('tramite', 'estudiante_tramite.id_tramite', '=', 'tramite.id_tramite')
+            ->join('estado', 'estudiante_tramite.id_estado', '=', 'estado.id_estado')
+            ->join('entidad', 'estudiante_tramite.id_entidad', '=', 'entidad.id_entidad')
+            ->select( $selectColumnsCambioCarrera )
+            ->where( 'estudiante_tramite.id_entidad', '=' , 4 ) // FIXME: DATOS QUEMADO
+            ->where( 'cambio_carrera.id_carrera_destino', '=', $idCarrera)
+            ->where( 'estudiante_tramite.activo', '=' , true )
+            ->whereIn( 'estudiante_tramite.id_estado', $arrayEstados )
+            ->orderBy( 'estudiante_tramite.fecha_proceso' , 'DESC');
+            // ->union( $estudianteAnulaciones )
+            // ->union( $estudianteCambiosCarreraOrigen )
+            // ->get();
+
+        ///////////////////
+
+        $selectColumnsTransferencia = [
+            'estudiante.id_estudiante as idEstudiante',
+            'estudiante.ru',
+            'estudiante.ci',
+            'estudiante.complemento',
+            'estudiante.paterno',
+            'estudiante.materno',
+            'estudiante.nombres',
+            'estudiante.fecha_nacimiento AS fechaNacimiento',
+            'estudiante.sexo',
+
+            'transferencia.id_transferencia AS idTipoTramite',
+            'transferencia.fecha_solicitud AS fechaSolicitud',
+            'transferencia.id_carrera_origen AS idCarreraOrigen',
+            'carrera.nombre AS carrera',
+            'transferencia.motivo',
+
+            'estudiante_tramite.id_estudiante_tramite as idEstudianteTipoTramiteTablaIntermedia',
+            'estudiante_tramite.fecha_proceso AS fechaProceso',
+            'estudiante_tramite.observaciones',
+
+            'tramite.id_tramite AS idTramite',
+            'tramite.descripcion AS tramite',
+
+            'estado.id_estado AS idEstado',
+            'estado.descripcion AS estado',
+
+            'entidad.id_entidad AS idEntidad',
+            'entidad.descripcion AS entidad'
+        ];
+
+        $estudianteTransferenciasOrigen = DB::table('estudiante')
+        ->join('estudiante_carrera', 'estudiante_carrera.id_estudiante', '=' , 'estudiante.id_estudiante')
+        ->join('carrera', 'carrera.id_carrera', '=' , 'estudiante_carrera.id_carrera')
+        ->join('estudiante_tramite', 'estudiante_tramite.id_estudiante', '=', 'estudiante.id_estudiante' )
+        ->join('transferencia', 'transferencia.id_transferencia', '=', 'estudiante_tramite.id_transferencia')
+
+        ->join('tramite', 'estudiante_tramite.id_tramite', '=', 'tramite.id_tramite')
+        ->join('estado', 'estudiante_tramite.id_estado', '=', 'estado.id_estado')
+        ->join('entidad', 'estudiante_tramite.id_entidad', '=', 'entidad.id_entidad')
+        ->select( $selectColumnsTransferencia )
+        ->where( 'estudiante_tramite.id_entidad', '=' , Entidad::DIRECTOR_CARRERA_ORIGEN )
+        ->where( 'transferencia.id_carrera_origen', '=', $idCarrera)
+        ->where( 'estudiante_tramite.activo', '=' , true )
+        ->whereIn( 'estudiante_tramite.id_estado', $arrayEstados )
+        ->orderBy( 'estudiante_tramite.fecha_proceso' , 'DESC');
+
+
+        $estudianteTransferenciasDestino = DB::table('estudiante')
+        ->join('estudiante_carrera', 'estudiante_carrera.id_estudiante', '=' , 'estudiante.id_estudiante')
+        ->join('carrera', 'carrera.id_carrera', '=' , 'estudiante_carrera.id_carrera')
+        ->join('estudiante_tramite', 'estudiante_tramite.id_estudiante', '=', 'estudiante.id_estudiante' )
+        ->join('transferencia', 'transferencia.id_transferencia', '=', 'estudiante_tramite.id_transferencia')
+
+        ->join('tramite', 'estudiante_tramite.id_tramite', '=', 'tramite.id_tramite')
+        ->join('estado', 'estudiante_tramite.id_estado', '=', 'estado.id_estado')
+        ->join('entidad', 'estudiante_tramite.id_entidad', '=', 'entidad.id_entidad')
+        ->select( $selectColumnsTransferencia )
+        ->where( 'estudiante_tramite.id_entidad', '=' , Entidad::DIRECTOR_CARRERA_DESTINO )
+        ->where( 'transferencia.id_carrera_destino', '=', $idCarrera)
+        ->where( 'estudiante_tramite.activo', '=' , true )
+        ->whereIn( 'estudiante_tramite.id_estado', $arrayEstados )
+        ->orderBy( 'estudiante_tramite.fecha_proceso' , 'DESC')
+        ->union( $estudianteAnulaciones )
+        ->union( $estudianteCambiosCarreraOrigen )
+        ->union( $estudianteCambiosCarreraDestino )
+        ->union( $estudianteTransferenciasOrigen )
+        ->get();
+
+
+
+
+
+
+        return response()->json([
+            'data'    => $estudianteTransferenciasDestino->isEmpty() ? null : $estudianteTransferenciasDestino,
+            'message' => $estudianteTransferenciasDestino->isEmpty() ? 'NO SE ENCONTRARON RESULTADOS' : 'SE ENCONTRARON RESULTADOS',
+            'error'   => null
+        ]);
+    }
 }
