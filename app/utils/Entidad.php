d1755cee300c2a20e8e102be4d5a2de54aa63481
Marco Antonio Molina López <marco.molina@segip.gob.bo>
2021-06-10 18:45:33 -0400

    Creacion de la clase: Entidad.

---
+++ app/utils/Entidad.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\utils;
+
+class Entidad{
+
+    public const ESTUDIANTE               = 1;
+    public const ENCARGADO_DAR            = 2;
+    public const DIRECTOR_CARRERA_ORIGEN  = 3;
+    public const DIRECTOR_CARRERA_DESTINO = 4;
+    public const ADMINISTRADOR            = 5;
+}
